0f561bb299e36e9e932464d222f93f7471ab8c18
[Lembe] <[bruzzone123@gmail.com]>
2024-10-12 21:02:34 -0500

    Implementacón de imagenes

---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import DoctorCinturaParaArriba from './imagenes de prueba/DoctorCinturaParaArriba.jpg'
+import DocotorEnfermero from './imagenes de prueba/DocotorEnfermero.jpg'
+import DoctorSinFonodo from './imagenes de prueba/DoctorSinFonodo.png'
+import Doctorsonriendo from './imagenes de prueba/Doctorsonriendo.png'
+
+
+export const assets ={
+    DoctorCinturaParaArriba,
+    DocotorEnfermero,
+    DoctorSinFonodo,
+    Doctorsonriendo
+}
