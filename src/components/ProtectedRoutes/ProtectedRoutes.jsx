9c16be3c61deccf4e9ec83434d51421374c38e99
Borys1no <boryscereceda@hotmail.com>
2024-10-20 12:34:57 -0500

    carpetas del proyecto eliminar el archivo txt

---
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/authContext";
+import { Navigate } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+const ProtectedRoute = ({ role, children }) => {
+  const { currentUser } = useAuth();
+  const [userRole, setUserRole] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      if (currentUser) {
+        const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+        if (userDoc.exists()) {
+          setUserRole(userDoc.data().role);
+        } else {
+          console.error("No se encontro el documento del usuario. ");
+        }
+      }
+      setLoading(false);
+    };
+    fetchUserRole();
+  }, [currentUser]);
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  if (!currentUser) {
+    return <Navigate to="/login" replace />;
+  }
+  if (userRole !== role) {
+    return <Navigate to="/home" replace />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
